6eae412430ac74038ab5d35baa09606e688507a3
Raulete <raulahernandez9@gmail.com>
2024-12-12 18:33:17 -0300

    Primer Commit

---
+++ src/test/java/com/rauldev/literAlura/LiterAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rauldev.literAlura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
